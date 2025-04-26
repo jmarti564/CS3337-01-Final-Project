e31290d49c25098a39fb45392bda0e29c83cd4c9
Jesus Calvo <jcalvo7@calstatela.edu>
2025-04-26 15:03:16 -0700

    Revamp bookEx 4/26
    (1) About Us (incomplete - about.html)
    (2) Search a book
    (3) Reformat - bootstrap/js
    
    additional features
    (6) comments - incomplete
    (7) rate - not created
    (8) Favorite list - not created
    
    If someone else has a different project they would prefer to start from. Please clone and upload

---
+++ bookEx/bookMng/models.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+
+
+class MainMenu(models.Model):
+    item = models.CharField(max_length=300, unique=True)
+    link = models.CharField(max_length=300, unique=True)
+
+    def __str__(self):
+        return self.item
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    web = models.URLField(max_length=300)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+    publishdate = models.DateField(auto_now=True)
+    picture = models.FileField(upload_to='bookEx/static/uploads')
+    pic_path = models.CharField(max_length=300, editable=False, blank=True)
+    username = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} on {self.book.name}"
